235204a5bea68045877934b68e8206de6dec425d
Max Maiers <maxmaiers@gmail.com>
2024-09-23 11:29:49 -0500

    intro

--- a2.py
+++ a2.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import re
 
 def match(pattern: List[str], source: List[str]) -> List[str]:
     """Attempts to match the pattern to the source.
@@ -14,39 +14,24 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         None if the pattern and source do not "match" ELSE A list of matched words
         (words in the source corresponding to _'s or %'s, in the pattern, if any)
     """
-    sind = 0  # current index we are looking at in source list
-    pind = 0  # current index we are looking at in pattern list
-    result: List[str] = []  # to store substitutions we will return if matched
+    result = []
 
-    # keep checking as long as we haven't hit the end of either pattern or source while
-    # pind is still a valid index OR sind is still a valid index (valid index means that
-    # the index is != to the length of the list)
-    while "FILL IN CONDITION HERE":
-        # your job is to fill out the body of this loop
+    inputBlank = pattern
+    inputFilled = source
 
-        # you should delete the following line
-        return ["Not done yet :)"]
+    if inputBlank == inputFilled:
+        return []
+    
+    cleanedBlank = [re.sub("%", "", s) for s in inputBlank]
+    cleanedBlank = [re.sub("_", "", s) for s in cleanedBlank]
 
-        # 1) if we reached the end of the pattern but not source
-
-        # 2) if the current thing in the pattern is a %
-        # WARNING: this condition contains the bulk of the code for the assignment
-        # If you get stuck on this one, we encourage you to attempt the other conditions
-        #   and come back to this one afterwards
-
-        # 3) if we reached the end of the source but not the pattern
-
-        # 4) if the current thing in the pattern is an _
-
-        # 5) if the current thing in the pattern is the same as the current thing in the
-        # source
-
-        # 6) else : this will happen if none of the other conditions are met it
-        # indicates the current thing it pattern doesn't match the current thing in
-        # source
+    for i in range(len(cleanedBlank)):
+        if cleanedBlank[i] == "":
+            result =
 
     return result
 
+print(match(["x", "_", "_"], ["x", "y", "z"]))
 
 if __name__ == "__main__":
     assert match(["x", "y", "z"], ["x", "y", "z"]) == [], "test 1 failed"
